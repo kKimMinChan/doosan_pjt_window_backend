f159f0e0e0a927e7df656bb50dabf4d63605eea1
kKimMinChan <99105740+kKimMinChan@users.noreply.github.com>
2025-03-13 23:58:09 +0900

    wfe1

--- src/system-status/system-status.repository.ts
+++ src/system-status/system-status.repository.ts
@@ -17,11 +17,19 @@ export class SystemStatusMongoRepository implements SystemStatusRepository {
     private systemStatusModel: Model<SystemStatusDocument>,
   ) {}
 
+  // async update(systemStatus: SystemStatus) {
+  //   return await this.systemStatusModel.replaceOne(
+  //     {}, // ✅ 모든 문서를 대상으로 적용 (즉, 하나만 유지)
+  //     systemStatus, // ✅ 새로운 데이터로 교체
+  //     { upsert: true }, // ✅ 기존 문서가 없으면 새로 생성
+  //   );
+  // }
+
   async update(systemStatus: SystemStatus) {
-    return await this.systemStatusModel.replaceOne(
-      {}, // ✅ 모든 문서를 대상으로 적용 (즉, 하나만 유지)
+    return await this.systemStatusModel.findOneAndReplace(
+      {}, // ✅ 모든 문서를 대상으로 적용
       systemStatus, // ✅ 새로운 데이터로 교체
-      { upsert: true }, // ✅ 기존 문서가 없으면 새로 생성
+      { upsert: true, returnDocument: 'after' }, // ✅ 기존 문서가 없으면 생성 & 교체 후 새로운 값 반환
     );
   }
 }
